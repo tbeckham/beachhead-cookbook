64c1ea79a3e876c5214a82487ed58e923a0cb197
Matt Clark <mclark@eucalyptus.com>
2016-03-25 15:56:32 -0700

    Attribute cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
-
 ######################################################################################################################
 # beachhead base attributes
 ######################################################################################################################
@@ -15,20 +14,27 @@
 ######################################################################################################################
 ['beachhead']['dependency_sandbox_dir']="eucalyptus_dependencies.tar.gz"
 
-
 ######################################################################################################################
 # beachhead python environment
+#python modules to be installed into the python virtual environement
 ######################################################################################################################
-['beachhead']['calyptos_git_url']="https://github.com/eucalyptus/calyptos.git"
-['beachhead']['calyptos_branch']="master"
-['beachhead']['nephoria_git_url']="https://github.com/nephomaniac/nephoria.git"
-['beachhead']['nephoria_branch']="master"
-['beachhead']['adminapi_git_url']="https://github.com/nephomaniac/adminapi.git"
-['beachhead']['adminapi_branch']="master"
 
-['beachhead']['python_pkgs']=["calyptos", "adminapi"]
-
-
+['beachhead']['python_git_modules'] = {
+      "calyptos" => {'git_url'=> "https://github.com/eucalyptus/calyptos.git",
+                     'branch' => "master",
+                     'update' => true
+      },
+      "nephoria" => {'git_url'=> "https://github.com/nephomaniac/nephoria.git",
+                     'branch' => "master",
+                     'update' => true
+      },
+      "adminapi" => {'git_url'=> "https://github.com/nephomaniac/adminapi.git",
+                     'branch' => "master",
+                     'update' => true
+      },
+}
+# Extra python packages to be installed with pip into the virtual environment
+['beachhead']['extra_pip_pkgs']={"ipython" => true}
 
 ######################################################################################################################
 # beachhead rpms for building local repo and archive of Euca deployment packages
@@ -38,15 +44,14 @@
 #       "true": will attempt to download that package name w/o regard for version
 #       "false": will not download this package
 ######################################################################################################################
-['beachhead']['calyptos_git_url']="https://github.com/eucalyptus/calyptos.git"
-
+# Base yum packages to be downloaded into the local repo/archive
 ['beachhead']['system_rpms']={"gcc" => true,
-                             "python-virtualenv"=> true,
-                             "python-pip" => true,
-                             "python-devel" => true,
-                             "git" => true,
-                             "python-setuptools" => true}
-
+                              "python-virtualenv"=> true,
+                              "python-pip" => true,
+                              "python-devel" => true,
+                              "git" => true,
+                              "python-setuptools" => true}
+# Euca specific yum packages to be downloaded into the local repo/archive
 ['beachhead']['euca_rpms']={"eucalyptus" => "4.3.0" ,
                             "eucalyptus-admin-tool" => true,
                             "eucalyptus-axis2c-common" => true,
@@ -73,7 +78,7 @@
                             "eucalyptus-load-balancer-image" => true,
                             "eucanetd" => true,
                             "load-balancer-servo" => true}
-
+# Extra yum packages to be downloaded into the local repo/archive
 ['beachhead']['extra_rpms'] = {}
 
 
